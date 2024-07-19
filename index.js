6c43dc2fb66e43c48851b9c4e90178718c48c9f8
benson <mwangibenson923@gmail.com>
2024-07-19 16:24:31 +0300

    Adds index.js

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
+const mainElement = document.getElementById("main");
+mainElement.remove();
+
+const newHeader = document.createElement("h1");
+newHeader.id = "victory";
+newHeader.innerHTML = "YOUR-NAME is the champion"; // Replace YOUR-NAME with your name or desired text
+
+document.body.appendChild(newHeader); // Append newHeader to the document body or any desired location
+
+
+     
 // Write your code here!
